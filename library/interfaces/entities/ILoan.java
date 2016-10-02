04ffd1dc2c838d93aa38d6761103832a868b066f
chaudharyom <prakash21.om@gmail.com>
2016-10-03 02:37:50 +1100

    Update ILoan.java

--- library/interfaces/entities/ILoan.java
+++ library/interfaces/entities/ILoan.java
@@ -2,24 +2,21 @@ package library.interfaces.entities;
 
 import java.util.Date;
 
-public interface ILoan {
-	
-	public static final int LOAN_PERIOD = 14;
-	
-	
-	public void commit(int id);
-	
-	public void complete();
-	
-	public boolean isOverDue();
-	
-	public boolean checkOverDue(Date currentDate);
-	
-	public IMember getBorrower();
-	
-	public IBook getBook();
-	
-	public int getID();
-	
+public abstract interface ILoan {
 
+    public static final int LOAN_PERIOD = 14;
+
+    public abstract void commit(int paramInt);
+
+    public abstract void complete();
+
+    public abstract boolean isOverDue();
+
+    public abstract boolean checkOverDue(Date paramDate);
+
+    public abstract IMember getBorrower();
+
+    public abstract IBook getBook();
+
+    public abstract int getID();
 }
