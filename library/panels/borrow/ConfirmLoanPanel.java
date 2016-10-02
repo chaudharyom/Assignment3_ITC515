6123ec343352310cd6c6433be780fdea2d246e6f
chaudharyom <prakash21.om@gmail.com>
2016-10-03 02:55:00 +1100

    Update ConfirmLoanPanel.java

--- library/panels/borrow/ConfirmLoanPanel.java
+++ library/panels/borrow/ConfirmLoanPanel.java
@@ -1,81 +1,73 @@
 package library.panels.borrow;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.TitledBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import library.interfaces.IBorrowUIListener;
 
-import java.awt.Font;
+public class ConfirmLoanPanel
+        extends ABorrowPanel {
 
-public class ConfirmLoanPanel extends ABorrowPanel {
+    private static final long serialVersionUID = 1L;
+    private JTextArea loanListTA;
 
-	private static final long serialVersionUID = 1L;
-	private JTextArea loanListTA;
+    public ConfirmLoanPanel(final IBorrowUIListener listener) {
+        setLayout(null);
+        setBorder(new TitledBorder(null, "Confirm Loans", 4, 2, null, null));
+        setBounds(12, 23, 460, 640);
 
-	/**
-	 * Create the panel.
-	 */
-	public ConfirmLoanPanel(IBorrowUIListener listener) {
-		setLayout(null);
-		setBorder(new TitledBorder(null, "Confirm Loans", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		setBounds(12, 23, 460, 640);
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Current Loan List", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(12, 24, 415, 496);
-		this.add(panel);
-		panel.setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 20, 395, 463);
-		panel.add(scrollPane);
-		
-		loanListTA = new JTextArea();
-		loanListTA.setEditable(false);
-		scrollPane.setViewportView(loanListTA);
+        JPanel panel = new JPanel();
+        panel.setBorder(new TitledBorder(null, "Current Loan List", 4, 2, null, null));
+        panel.setBounds(12, 24, 415, 496);
+        add(panel);
+        panel.setLayout(null);
 
-		JButton btnReject = new JButton("Reject");
-		btnReject.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnReject.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				listener.loansRejected();
-			}
-		});
-		btnReject.setBounds(173, 533, 115, 35);
-		this.add(btnReject);
-		
-		JButton btnConfirm = new JButton("Confirm");
-		btnConfirm.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnConfirm.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				listener.loansConfirmed();
-			}
-		});
-		btnConfirm.setBounds(30, 533, 115, 35);
-		this.add(btnConfirm);
-		
-		JButton button = new JButton("Cancel");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				listener.cancelled();
-			}
-		});
-		button.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		button.setBounds(312, 533, 115, 35);
-		add(button);
-	}
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(10, 20, 395, 463);
+        panel.add(scrollPane);
 
+        this.loanListTA = new JTextArea();
+        this.loanListTA.setEditable(false);
+        scrollPane.setViewportView(this.loanListTA);
 
-	@Override
-	public void displayConfirmingLoan(String loanDetails) {
-		loanListTA.setText(loanDetails);
-		loanListTA.setCaretPosition(0);
-	}
+        JButton btnReject = new JButton("Reject");
+        btnReject.setFont(new Font("Tahoma", 0, 14));
+        btnReject.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                listener.loansRejected();
+            }
+        });
+        btnReject.setBounds(173, 533, 115, 35);
+        add(btnReject);
 
+        JButton btnConfirm = new JButton("Confirm");
+        btnConfirm.setFont(new Font("Tahoma", 0, 14));
+        btnConfirm.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                listener.loansConfirmed();
+            }
+        });
+        btnConfirm.setBounds(30, 533, 115, 35);
+        add(btnConfirm);
 
+        JButton button = new JButton("Cancel");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                listener.cancelled();
+            }
+        });
+        button.setFont(new Font("Tahoma", 0, 14));
+        button.setBounds(312, 533, 115, 35);
+        add(button);
+    }
+
+    public void displayConfirmingLoan(String loanDetails) {
+        this.loanListTA.setText(loanDetails);
+        this.loanListTA.setCaretPosition(0);
+    }
 }
