0af232387f71efeffd1ff2bdd2b9b660ef49b59b
chaudharyom <prakash21.om@gmail.com>
2016-10-03 02:29:57 +1100

    Update ILoanHelper.java

--- library/interfaces/daos/ILoanHelper.java
+++ library/interfaces/daos/ILoanHelper.java
@@ -1,13 +1,11 @@
 package library.interfaces.daos;
 
 import java.util.Date;
-
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
-public interface ILoanHelper {
-	
-	public ILoan makeLoan(IBook book, IMember borrower, Date borrowDate, Date dueDate);
+public abstract interface ILoanHelper {
 
+    public abstract ILoan makeLoan(IBook paramIBook, IMember paramIMember, Date paramDate1, Date paramDate2);
 }
